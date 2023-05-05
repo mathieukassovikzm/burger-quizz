d75da73297afff3c49059acd0897db9cba5ab4c2
Mathieu le bellec <mathieu.le.bellec@gmail.com>
2023-05-05 14:00:33 +0200

    feat: init services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,91 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// const routes = [
+//   { path: '/', component: PageHome, name: 'p-home' },
+//   { path: '/intro', component: PageIntro, name: 'pageIntro' },
+//   {
+//     path: '/nuggets-equipes',
+//     component: PageChoixDesEquipes,
+//     name: 'pageChoixEquipes',
+//   },
+//   {
+//     path: '/nuggets-rules',
+//     component: PageNuggetsRules,
+//     name: 'pageNuggetsRules',
+//   },
+//   {
+//     path: '/nuggets-video',
+//     component: PageNuggetsVideo,
+//     name: 'pageNuggetsVideo',
+//   },
+//   {
+//     path: '/sel-poivre-rules',
+//     component: PageSelPoivreRules,
+//     name: 'pageSelPoivreRules',
+//   },
+//   {
+//     path: '/sel-poivre-video',
+//     component: PageSelPoivreVideo,
+//     name: 'pageSelPoivreVideo',
+//   },
+//   { path: '/menu-rules', component: PageMenusRules, name: 'pageMenusRules' },
+//   { path: '/menu-video', component: PageMenusVideo, name: 'pageMenusVideo' },
+//   {
+//     path: '/dessert-rules',
+//     component: PageDessertRules,
+//     name: 'pageDessertRules',
+//   },
+//   {
+//     path: '/addition-rules',
+//     component: PageAdditionRules,
+//     name: 'pageAdditionRules',
+//   },
+//   {
+//     path: '/addition-video',
+//     component: PageAdditionVideo,
+//     name: 'pageAdditionVideo',
+//   },
+//   {
+//     path: '/burger-de-la-mort-rules',
+//     component: PageBurgerDeLaMortRules,
+//     name: 'pageBurger2laMortRules',
+//   },
+//   {
+//     path: '/burger-de-la-mort-video',
+//     component: PageBurgerDeLaMortVideo,
+//     name: 'pageBurger2laMortVideo',
+//   },
+//   {
+//     path: '/burger-de-la-mort-mayo',
+//     component: PageBurgerDeLaMortMayo,
+//     name: 'pageBurger2laMortMayo',
+//   },
+//   {
+//     path: '/burger-de-la-mort-ketchup',
+//     component: PageBurgerDeLaMortKetchup,
+//     name: 'pageBurger2laMortKetchup',
+//   },
+//   {
+//     path: '/fin',
+//     component: PageFin,
+//     name: 'pageFin',
+//   },
+//   {
+//     path: '/question',
+//     component: ScreenQuestion,
+//     name: 'screen-question',
+//     children: [
+//       { path: '/question/:id', component: Questions, name: 'question' },
+//     ],
+//   },
+//   { path: '*', redirect: '/' },
+// ];
+
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
